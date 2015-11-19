fafe440276578cb9b0bdae6caba14caf1c09176e
Vishnu Mohan <vishnu@mesosphere.com>
2015-11-19 09:13:55 -0500

    Use source bin/env-setup instead of source bin/activate

--- dcos.sh
+++ dcos.sh
@@ -3,11 +3,11 @@
 if [ -z "$1" ]; then
   echo "Note: You can specify the URL of your DCOS cluster upon startup"
   echo "e.g., docker run -i -t vishnumohan/alpine-dcoscli http://dcos.elb.amazonaws.com"
-  source bin/activate
+  source bin/env-setup
   echo ""
 elif [ ! -z "$1" ]; then
   echo "Setting core.dcos_url set to ${1}"
-  source bin/activate && dcos config set core.dcos_url "$1"
+  source bin/env-setup && dcos config set core.dcos_url "$1"
   echo ""
 fi
 
